2cbfe2c2d3a725f052e69bb109d9e49af64d5cfb
unknown <mokdavid14@gmail.com>
2015-09-19 22:53:21 +1200

    Adjust images and links

--- assets/scripts/ChineseZodiac.json.js
+++ assets/scripts/ChineseZodiac.json.js
@@ -2,28 +2,28 @@
 	"name": "Rat",
 	"subtitle": "鼠",
 	"years":[1948,1960,1972,1984,1996,2008],
-	"icon": "assets/images/chinese/rat.jpg",
+	"icon": "assets/images/chinese/Rat.jpg",
 	"description": "Rats are quick-witted, resourceful and versatile. With strong intuition and quick response, they always easily adapt themselves to a new environment."
 },
 { 
 	"name": "Ox",
 	"subtitle": "牛",
 	"years":[1949,1961,1973,1985,1997,2009] ,
-	"icon": "assets/images/chinese/ox.jpg",
+	"icon": "assets/images/chinese/Ox.jpg",
 	"description": "Oxes are known for diligence, dependability, strength and determination. Having an honest nature, Oxes have ideals and ambitions for life, and attach importance to family and work."
 },
 { 
 	"name": "Tiger",
 	"subtitle": "虎",
 	"years":[1950,1962,1974,1986,1998,2010] ,
-	"icon": "assets/images/chinese/tiger.jpg",
+	"icon": "assets/images/chinese/Tiger.jpg",
 	"description": "Tigers are brave, competitive, unpredictable, and self-confident. They are very charming and well-liked by others."
 },
 { 
 	"name": "Rabbit",
 	"subtitle": "兔",
 	"years":[1951,1963,1975,1987,1999,2011],
-	"icon": "assets/images/chinese/rabbit.jpg",
+	"icon": "assets/images/chinese/Rabbit.jpg",
 	"description": "Rabbits tend to be gentle, quiet, elegant, and alert; quick, skillful, kind, and patient; and particularly responsible."
 },
 
@@ -31,28 +31,28 @@
 	"name": "Dragon",
 	"subtitle": "龍",
 	"years":[1952,1964,1976,1988,2000,2012] ,
-	"icon": "assets/images/chinese/dragon.jpg",
+	"icon": "assets/images/chinese/Dragon.jpg",
 	"description": "Gifted with innate courage, tenacity and intelligence, Dragons are enthusiastic and confident. They are not afraid of challenges, and willing to take risks."
 },
 { 
 	"name": "Snake",
 	"subtitle": "蛇",
 	"years":[1953,1965,1977,1989,2001,2013] ,
-	"icon": "assets/images/chinese/snake.jpg",
+	"icon": "assets/images/chinese/Snake.jpg",
 	"description": "Snakes tend to act according to their own judgments, even while remaining the most private and reticent. They are determined to accomplish their goals and hate to fail."
 },
 { 
 	"name": "Horse",
 	"subtitle": "馬",
 	"years":[1954,1966,1978,1990,2002,2014] ,
-	"icon": "assets/images/chinese/horse.jpg",
+	"icon": "assets/images/chinese/Horse.jpg",
 	"description": "People born in a year of the Horse are extremely animated, active and energetic. They also love to be in a crowd."
 },
 { 	
 	"name": "Goat",
 	"subtitle": "羊",
 	"years":[1955,1967,1979,1991,2003,2015] ,
-	"icon": "assets/images/chinese/goat.jpg",
+	"icon": "assets/images/chinese/Goat.jpg",
 	"description": "Goats are generally believed to be gentle mild-mannered, shy, stable, sympathetic, amicable, and brimming with a strong sense of kindheartedness and justice."
 },
 
@@ -60,27 +60,27 @@
 	"name": "Monkey",
 	"subtitle": "猴",
 	"years":[1944,1956,1968,1980,1992,2004] ,
-	"icon": "assets/images/chinese/monkey.jpg",
+	"icon": "assets/images/chinese/Monkey.jpg",
 	"description": "Monkeys are fast learners and crafty opportunists. They have many interests and need partners who are capable of stimulating them."
 },
 { 
 	"name": "Rooster",
 	"subtitle": "雞",
 	"years":[1945,1957,1969,1981,1993,2005] ,
-	"icon": "assets/images/chinese/rooster.jpg",
+	"icon": "assets/images/chinese/Rooster.jpg",
 	"description": "Hardworking, resourceful, courageous and talented, Roosters are very confident about themselves. Likes to be the center of attention."
 },
 { 
 	"name": "Dog",
 	"subtitle": "狗",
 	"years":[1946,1958,1970,1982,1994,2006] ,
-	"icon": "assets/images/chinese/dog.jpg",
+	"icon": "assets/images/chinese/Dog.jpg",
 	"description": "Dogs are loyal and honest, amiable and kind, cautious and prudent. Cares a lot for the people whom they think are most important."
 },
 { 
 	"name": "Boar",
 	"subtitle": "豬",
 	"years":[1947,1959,1971,1983,1995,2007],
-	"icon": "assets/images/chinese/boar.jpg",
+	"icon": "assets/images/chinese/Boar.jpg",
 	"description": "Pigs are diligent, compassionate, and generous. They have great concentration: once they set a goal, they will devote all their energy to achieving it."
 }]
--- assets/scripts/GreekZodiac.json.js
+++ assets/scripts/GreekZodiac.json.js
@@ -3,7 +3,7 @@
 	"subtitle": "The Ram",
 	"startDate": "0000-03-20",
 	"endDate": "0000-04-20",
-	"icon": "assets/images/greek/aries.jpg",
+	"icon": "assets/images/greek/Aries.jpg",
 	"description": "Aries are creative, adaptive, and insightful. They can also be strong-willed and spontaneous (sometimes to a fault)."
 },
 { 
@@ -11,7 +11,7 @@
 	"subtitle": "The Bull",
 	"startDate": "0000-04-20",
 	"endDate": "0000-05-21",
-	"icon": "assets/images/greek/taurus.jpg",
+	"icon": "assets/images/greek/Taurus.jpg",
 	"description": "Stubborn by nature, Tauruses will stand their ground to the bitter end. But that's okay because the Taurus is also a loving, sympathetic and appreciative sign."
 },
 { 
@@ -19,7 +19,7 @@
 	"subtitle": "The Twins",
 	"startDate": "0000-05-21",
 	"endDate": "0000-06-21",
-	"icon": "assets/images/greek/gemini.jpg",
+	"icon": "assets/images/greek/Gemini.jpg",
 	"description": "Geminis tend to have a duality to their nature, and may be tough to predict how they will react."
 },
 { 
@@ -27,7 +27,7 @@
 	"subtitle": "The Crab",
 	"startDate": "0000-06-21",
 	"endDate": "0000-07-23",
-	"icon": "assets/images/greek/cancer.jpg",
+	"icon": "assets/images/greek/Cancer.jpg",
 	"description": "Cancers love home-life, family and domestic settings. They are traditionalists, and enjoy operating on a fundamental level."
 },
 
@@ -36,7 +36,7 @@
 	"subtitle": "The Lion",
 	"startDate": "0000-07-23",
 	"endDate": "0000-08-23",
-	"icon": "assets/images/greek/leo.jpg",
+	"icon": "assets/images/greek/Leo.jpg",
 	"description": "Leos are natural born leaders, and they will let you know it as they have a tendency to be high-minded and vocal about their opinions."
 },
 { 
@@ -44,7 +44,7 @@
 	"subtitle": "The Maiden",
 	"startDate": "0000-08-23",
 	"endDate": "0000-09-23",
-	"icon": "assets/images/greek/virgo.jpg",
+	"icon": "assets/images/greek/Virgo.jpg",
 	"description": "Virgos have keen minds, and are delightful to chat with, often convincing others of outlandish tales with ease and charm."
 },
 { 
@@ -52,7 +52,7 @@
 	"subtitle": "The Scales",
 	"startDate": "0000-09-23",
 	"endDate": "0000-10-23",
-	"icon": "assets/images/greek/libra.jpg",
+	"icon": "assets/images/greek/Libra.jpg",
 	"description": "Libras have keen intuitions, but often don't give themselves enough credit for their perceptions. They can be quiet and shy if not persuaded to come out of their shell."
 },
 { 	
@@ -60,7 +60,7 @@
 	"subtitle": "The Scorpion",
 	"startDate": "0000-10-23",
 	"endDate": "0000-11-22",
-	"icon": "assets/images/greek/scorpio.jpg",
+	"icon": "assets/images/greek/Scorpio.jpg",
 	"description": "Scorpios are bold and are capable of executing massive enterprises with cool control and confidence, and have unshakable focus when the situation calls for it."
 },
 
@@ -69,7 +69,7 @@
 	"subtitle": "The Archer",
 	"startDate": "0000-11-22",
 	"endDate": "0000-12-22",
-	"icon": "assets/images/greek/sagittarius.jpg",
+	"icon": "assets/images/greek/Sagittarius.jpg",
 	"description": "Sagittarii have great ability for focus, and can be very intense. However, they are not very patient and expect quick results."
 },
 { 
@@ -77,7 +77,7 @@
 	"subtitle": "The Sea-Goat",
 	"startDate": "0000-12-22",
 	"endDate": "0000-01-20",
-	"icon": "assets/images/greek/capricorn.jpg",
+	"icon": "assets/images/greek/Capricorn.jpg",
 	"description": "Capricorns apply their knowledge to practical matters, and strive to maintain stability and order, and make good organizers too."
 },
 { 
@@ -85,7 +85,7 @@
 	"subtitle": "The Water-Bearer",
 	"startDate": "0000-01-20",
 	"endDate": "0000-02-19",
-	"icon": "assets/images/greek/aquarius.jpg",
+	"icon": "assets/images/greek/Aquarius.jpg",
 	"description": "Often simple and unassuming, Aquariuses go about accomplishing goals in quiet, often unorthodox ways, but the results surprisingly effective."
 },
 { 
@@ -93,6 +93,6 @@
 	"subtitle": "The Fish",
 	"startDate": "0000-02-19",
 	"endDate": "0000-03-20",
-	"icon": "assets/images/greek/pisces.jpg",
+	"icon": "assets/images/greek/Pisces.jpg",
 	"description": "Pisces keep an extremely low profile compared to others in the zodiac. They are honest, unselfish, trustworthy and often have quiet dispositions."
 }]
